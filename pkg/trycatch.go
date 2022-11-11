2b17d31453b6fec5196caf9caeef1987218d881b
Pouya Vedadiyan <vedadiyan@gmail.com>
2022-11-11 17:26:28 +0800

    Naming Fix

--- pkg/trycatch.go
+++ pkg/trycatch.go
@@ -1,25 +1,25 @@
 package gotrycatch
 
-type Trial[TOutput any] struct {
+type trial[TOutput any] struct {
 	functions []func(arg any) (any, error)
 	result    any
 }
 
-func New[TOutput any]() *Trial[TOutput] {
-	trial := &Trial[TOutput]{
+func Try[TOutput any]() *trial[TOutput] {
+	trial := &trial[TOutput]{
 		functions: make([]func(arg any) (any, error), 0),
 	}
 	return trial
 }
 
-func (trial Trial[TOutput]) UnWrap() TOutput {
+func (trial trial[TOutput]) UnWrap() TOutput {
 	if out, ok := trial.result.(TOutput); ok {
 		return out
 	}
 	return *new(TOutput)
 }
 
-func (trial *Trial[TOutput]) Catch() error {
+func (trial *trial[TOutput]) Catch() error {
 	var _arg any
 	for _, value := range trial.functions {
 		res, err := value(_arg)
@@ -32,7 +32,7 @@ func (trial *Trial[TOutput]) Catch() error {
 	return nil
 }
 
-func Try[TInput any, TOutput any](trial *Trial[TOutput], fn func(arg TInput) (any, error)) {
+func Continue[TInput any, TOutput any](trial *trial[TOutput], fn func(arg TInput) (any, error)) {
 	trial.functions = append(trial.functions, func(arg any) (any, error) {
 		if arg == nil {
 			_arg := new(TInput)
